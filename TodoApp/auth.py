8146eba0a127d35c0caff1961a317d604e8c18d0
danielel10 <elhadan@post.bgu.ac.il>
2023-12-25 19:25:13 +0200

    finished jwt implementation

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from typing import Optional
 import models
@@ -56,26 +56,26 @@ def authenticate_user(username: str, password: str, db: Session = Depends(get_db
 
 
 def create_access_token(username: str, user_id: int,
-                        exires_delta: Optional[timedelta] = None):
+                        expires_delta: Optional[timedelta] = None):
     encode = {"sub": username, "id": user_id}
-    if exires_delta:
-        expire = datetime.utcnow()
+    if expires_delta:
+        expire = datetime.utcnow() + timedelta(hours=2)
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     encode.update({"exp": expire})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
-async def get_current_user(token: str = Depends(oauth2_bearer)):
+def get_current_user(token: str = Depends(oauth2_bearer)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         user_id: str = payload.get("id")
         if username is None or user_id is None:
-            raise HTTPException(status_code=404, dateil="User not found")
+            raise get_user_exception()
         return {"username": username, "id": user_id}
     except JWTError:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise get_user_exception()
 
 
 @app.post("/create/user")
@@ -98,8 +98,23 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
                                  db: Session = Depends(get_db)):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise token_exception()
     token_expires = timedelta(minutes=20)
-    token = create_access_token(user.username, user.id, exires_delta=token_expires)
+    token = create_access_token(user.username, user.id, expires_delta=token_expires)
 
     return {"token": token}
+
+
+# Exceptions
+def get_user_exception():
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail="could not validate credentials",
+                                          headers={"WWW-Authenticate": "Bearer"})
+    return credentials_exception
+
+
+def token_exception():
+    token_exception_repsonse = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                             detail="Incorrect username or password",
+                                             headers={"WWW-Authenticate": "Bearer"})
+    return token_exception_repsonse
